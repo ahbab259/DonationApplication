ea2c882eb5b01a229ac179b382042534d2994bf9
ahbab259 <hasin_ahbab@yahoo.com>
2023-06-20 12:17:06 -0400

    Bug Fixes

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -25,9 +26,29 @@ namespace DonationApplication.Models
         [Required]
         [DisplayName("USER_NAME")]
         public string USER_NAME { get; set; }
-        [Required]
-        [DisplayName("PASSWORD")]
+
+
+        //[Required]
+        //[DisplayName("PASSWORD")]
+        //[DataType(DataType.Password)]
+        //[StringLength(100, ErrorMessage = "Password \"{0}\" must have {2} character", MinimumLength = 8)]
+        //[RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{6,}$", ErrorMessage = "Password must contain: Minimum 8 characters atleast 1 UpperCase Alphabet, 1 LowerCase Alphabet, 1 Number and 1 Special Character")]
+        [Required(ErrorMessage = "*Required Field")]
+        [DataType(DataType.Password)]
+        [MaxLength(15, ErrorMessage = "Password cannot be longer than 15 characters.")]
         public string PASSWORD { get; set; }
+
+
+
+        [NotMapped] // Does not effect with your database
+        [Required(ErrorMessage = "Please enter confirm password")]
+        [Compare("Password", ErrorMessage = "Confirm password doesn't match, Type again !")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+
+
+
         [Required]
         [DisplayName("USER_TYPE *")]
         public string USER_TYPE { get; set; }
